9e198141268d01fbdfa01dbba093f598b63d4434
Diego Pasaye <pasayealvarado@gmail.com>
2022-12-28 01:52:42 -0600

    responsive

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const share = document.querySelector(".shareIcon")
 const icons = document.querySelector(".icons")
+const container = document.querySelector(".shareButton")
 estado = "apagado"
 
 const mostrar = () => {
     icons.style.opacity = "1"
+    container.style.background = "hsl(217, 19%, 35%)"
     estado = "activo"
 }
 const ocultar = () => {
     if(estado == "activo"){
         icons.style.opacity = "0"
+        container.style.background = ""
         estado = "apagado"
     }
 }
